60fa21cae5c8f8a056d8c83fe73d1e30665b9f67
Gaëtan <gaetan.hrlt@gmail.com>
2023-06-11 17:20:48 +0200

    dogs!

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Instagram Story Daily Cat
-Wanna post a cat everyday in your Instagram story? This repo is made for you!
+# Instagram Story Daily Dog
+Wanna post a dog everyday in your Instagram story? This repo is made for you!
 
 
 # Installation as a Linux service
 Download
 ```
-git clone https://github.com/ghrlt/instagram-daily-cat.git
-cd instagram-daily-cat
+git clone https://github.com/ghrlt/instagram-daily-dog.git
+cd instagram-daily-dog
 ```
 
 Install requirements
@@ -14,11 +14,11 @@ Install requirements
 pip install -r requirements.txt
 ```
 
-Before continuing, you will have to set the value of `WorkingDirectory` in `ig-dailycat.service` file by the path to the location of ig-dailycat folder.
+Before continuing, you will have to set the value of `WorkingDirectory` in `ig-dailydog.service` file by the path to the lodogion of ig-dailydog folder.
 Once you did, and only then, you can continue
 ```
-mv ig-dailycat.service /etc/systemd/system
-mv ig-dailycat.timer /etc/systemd/system
+mv ig-dailydog.service /etc/systemd/system
+mv ig-dailydog.timer /etc/systemd/system
 ```
 
 Now open `.env` in your preferred editor and fill in your details. (If you do not have 2FA on your account, do not touch `INSTAGRAM_2FA_SEED`.
@@ -26,15 +26,15 @@ Now open `.env` in your preferred editor and fill in your details. (If you do no
 Then, enable the service.
 ```
 systemctl daemon-reload
-systemctl enable ig-dailycat --now
+systemctl enable ig-dailydog --now
 ```
-Enjoy your daily cat, default posted at 5pm UTC
+Enjoy your daily dog, default posted at 5pm UTC
 
 # FAQ
 
 #### What is 'INSTAGRAM_2FA_SEED'?
-If you have enabled 2FA on your Instagram account, and you set it up through an auth app like Authy or Google Authenticator, you were provided with a QRcode / a seed! 
-This seed allow to generate the 2FA 6 digits code, required to login in your account. You can found it, either when setting up 2FA or in your auth application (Usually, you'll have an "Edit" button on the account)
+If you have enabled 2FA on your Instagram account, and you set it up through an auth app like Authy or Google Authentidogor, you were provided with a QRcode / a seed! 
+This seed allow to generate the 2FA 6 digits code, required to login in your account. You can found it, either when setting up 2FA or in your auth applidogion (Usually, you'll have an "Edit" button on the account)
 
 #### Is it safe for my Instagram account?
 Automating anything on Instagram is against their TOS, thus, I cannot take any responsability if the usage of this program leads to a ban.
@@ -42,4 +42,4 @@ Automating anything on Instagram is against their TOS, thus, I cannot take any r
 However, the [library used](https://github.com/adw0rd/instagrapi) is built to avoid as much as possible any detection. Furthermore, as this program is only posting once a day, it shouldn't be flagged.
 
 
-If you are worried for your credentials, which you should always be, you can check by yourself the code of both this program and [instagrapi](https://github.com/ghrlt/instagram-daily-cat).
+If you are worried for your credentials, which you should always be, you can check by yourself the code of both this program and [instagrapi](https://github.com/ghrlt/instagram-daily-dog).
--- app.py
+++ app.py
@@ -28,61 +28,61 @@ if not INSTAGRAM_PASSWORD:
 INSTAGRAM_2FA_SEED = os.getenv('INSTAGRAM_2FA_SEED')
 
 
-def getCat(catid: int):
-    url = "https://cataas.com/api/cats?limit=1&skip={}".format(catid)
+def getDog(dogid: int):
+    url = "https://random.dog/doggos?include=jpg"
     response = requests.get(url)
 
     if response.status_code == 200:
-        url = "https://cataas.com/cat/{}".format(response.json()[0]['_id'])
+        url = "https://random.dog/{}".format(response.json()[dogid])
         response = requests.get(url)
 
         if response.status_code == 200:
-            catPath = WORKING_DIRECTORY_PATH + 'cat.jpg'
+            dogPath = WORKING_DIRECTORY_PATH + 'dog.jpg'
 
-            open(catPath, "wb").write(response.content)
-            return catPath
+            open(dogPath, "wb").write(response.content)
+            return dogPath
 
-    raise Exception("Error getting cat: {}".format(response.status_code))
+    raise Exception("Error getting dog: {}".format(response.status_code))
 
-def formatImage(catpath: str):
+def formatImage(dogpath: str):
     base = Image.new('RGB', (1080,1920), (255,255,0))
-    cat = Image.open(catpath)
-    originalCat = cat.copy()
+    dog = Image.open(dogpath)
+    originalDog = dog.copy()
 
-    #~ Resize and spawn multiple image of cat in the background
-    wPercent = (216/float(originalCat.size[0]))
-    hSize = int((float(originalCat.size[1])*float(wPercent)))
-    smallCat = originalCat.resize((216,hSize), Image.Resampling.LANCZOS)
+    #~ Resize and spawn multiple image of dog in the background
+    wPercent = (216/float(originalDog.size[0]))
+    hSize = int((float(originalDog.size[1])*float(wPercent)))
+    smallDog = originalDog.resize((216,hSize), Image.Resampling.LANCZOS)
 
-    #~ Reduce brightness & blur, goal is to put in foreground the main cat
-    smallCat = ImageEnhance.Brightness(smallCat).enhance(.75)
-    smallCat = smallCat.filter(ImageFilter.GaussianBlur(4))
+    #~ Reduce brightness & blur, goal is to put in foreground the main dog
+    smallDog = ImageEnhance.Brightness(smallDog).enhance(.75)
+    smallDog = smallDog.filter(ImageFilter.GaussianBlur(4))
 
     for i in range( math.ceil(base.size[1]/hSize) ):
-        for j in range( math.ceil(base.size[0]/smallCat.size[0]) ):
-            base.paste(smallCat, (j*smallCat.size[0],i*smallCat.size[1]))
+        for j in range( math.ceil(base.size[0]/smallDog.size[0]) ):
+            base.paste(smallDog, (j*smallDog.size[0],i*smallDog.size[1]))
 
 
     #~ Resize the image to fit, if it's too large (>1000) or too small (<800)
-    if cat.size[0] > 1000 or cat.size[0] < 800:
-        wPercent = (1000/float(cat.size[0]))
-        hSize = int((float(cat.size[1])*float(wPercent)))
-        cat = cat.resize((1000,hSize), Image.Resampling.LANCZOS)
+    if dog.size[0] > 1000 or dog.size[0] < 800:
+        wPercent = (1000/float(dog.size[0]))
+        hSize = int((float(dog.size[1])*float(wPercent)))
+        dog = dog.resize((1000,hSize), Image.Resampling.LANCZOS)
 
-    wPos = int((1080-cat.size[0])/2)
-    hPos = int((1920-cat.size[1])/2)
+    wPos = int((1080-dog.size[0])/2)
+    hPos = int((1920-dog.size[1])/2)
 
-    base.paste(cat, (wPos, hPos))
-    base.save(catpath, quality=95)
+    base.paste(dog, (wPos, hPos))
+    base.save(dogpath, quality=95)
 
-def addCaption(catpath: str, caption: str):
-    cat = Image.open(catpath)
-    draw = ImageDraw.Draw(cat)
+def addCaption(dogpath: str, caption: str):
+    dog = Image.open(dogpath)
+    draw = ImageDraw.Draw(dog)
     font = ImageFont.truetype(WORKING_DIRECTORY_PATH + 'captionFont.ttf', 80)
 
     #~ Determine text size & position
     _,_, wText, hText = draw.textbbox((0,0), caption, font=font)
-    wPos = (cat.size[0]-wText)/2
+    wPos = (dog.size[0]-wText)/2
 
     
     #~ Draw caption background
@@ -90,15 +90,15 @@ def addCaption(catpath: str, caption: str):
     #~ Vertical placement (top/bottom) is random
     onTop = random.randint(0,1) == 0
     if onTop:
-        hPos = (cat.size[1]-hText)/6
+        hPos = (dog.size[1]-hText)/6
         draw.rounded_rectangle((wPos*0.90, hPos*0.95, wPos*1.10+wText, hPos*1.10+hText), fill=(0,0,0), radius=20)
     else:
-        hPos = (cat.size[1]-hText)/1.1
+        hPos = (dog.size[1]-hText)/1.1
         draw.rounded_rectangle((wPos*0.90, hPos*0.985, wPos*1.10+wText, hPos*1.025+hText), fill=(0,0,0), radius=20) # Bottom
 
     #~ Write caption
     draw.text((wPos, hPos), caption, font=font, fill=(255,255,255))
-    cat.save(catpath)
+    dog.save(dogpath)
 
 
 def main():
@@ -117,29 +117,29 @@ def main():
             code = client.totp_generate_code(INSTAGRAM_2FA_SEED)
             print("Day #{} | Generated 2FA code".format(dayOfTheYear))
 
-    client.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD, verification_code=code)
+    client.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD, verifidogion_code=code)
     client.dump_settings(WORKING_DIRECTORY_PATH + 'ig.session')
 
     print("Day #{} | Logged in!".format(dayOfTheYear))
 
-    # Obtain the cat picture
-    catPath = getCat(dayOfTheYear)
-    print("Day #{} | Cat obtained!".format(dayOfTheYear))
+    # Obtain the dog picture
+    dogPath = getDog(dayOfTheYear)
+    print("Day #{} | Dog obtained!".format(dayOfTheYear))
 
     # Format the picture to fit
-    formatImage(catPath)
-    addCaption(catPath, 'Cat #{}'.format(dayOfTheYear))
+    formatImage(dogPath)
+    addCaption(dogPath, 'Dog #{}'.format(dayOfTheYear))
     print("Day #{} | Story formatted!".format(dayOfTheYear))
 
     # Post the picture in an Instagram story
-    catStory = client.photo_upload_to_story(catPath)
-    print("Day #{} | Cat posted!".format(dayOfTheYear))
+    dogStory = client.photo_upload_to_story(dogPath)
+    print("Day #{} | Dog posted!".format(dayOfTheYear))
 
     # Check if user as an highlight named "Cs" (Feel free to change this)
     for userHighlight in client.user_highlights(client.user_id):
-        if userHighlight.title == "Cats":
+        if userHighlight.title == "Dogs":
             # Add the story to highlights
-            client.highlight_add_stories(userHighlight.pk, [catStory.pk])
+            client.highlight_add_stories(userHighlight.pk, [dogStory.pk])
             print("Day #{} | Added story to highlight".format(dayOfTheYear))
 
 
